a6f3767d17745df9ebe5f334d41f5761f50f9270
huazui <2509155771@qq.com>
2019-07-14 22:34:51 +0800

    Add files via upload

--- src/main/java/com/thoughtworks/tdd/Dependency.java
+++ src/main/java/com/thoughtworks/tdd/Dependency.java
@@ -1,8 +1,28 @@
 package com.thoughtworks.tdd;
 
 public class Dependency {
-    public Dependency(){}
-    public String say(int number){
-        return String.valueOf(number);
+    public String fizzBuzz(int  i) {
+        if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+            return "FizzBuzzWhizz";
+        else if (i % 3 == 0 && i % 5 == 0) {
+            return "FizzBuzz";
+        } else if (i % 3 == 0 && i % 7 == 0) {
+            return "FizzWhizz";
+        } else if (i % 5 == 0 && i % 7 == 0) {
+            return "BuzzWhizz";
+        } else if (i % 3 == 0) {
+            return "Fizz";
+        } else if (i % 5 == 0)
+            return "Buzz";
+        else if (i % 7 == 0)
+            return "Whizz";
+        else {
+            return "" + i;
+        }
     }
+
+
+
+
+
 }
